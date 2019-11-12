a78d03ff3e3114294688c10f9430d13a8f6173b8
Jennifer Glauche <mail@jglauche.de>
2019-11-12 13:07:51 +0100

    add confy, add simple config file management for colors

--- src/cli.rs
+++ src/cli.rs
@@ -3,16 +3,72 @@ pub mod cli{
 	use crate::text_nav::*;
 	use std::error::Error;
 	use termion::{*,color};
+	use serde::{Serialize, Deserialize};
+
+	#[derive(Serialize, Deserialize)]
+	struct ColorConf {
+			version: u8,
+			header_bg: (u8, u8, u8),
+			header_fg: (u8, u8, u8),
+			text_bg: (u8, u8, u8),
+			text_fg: (u8, u8, u8),
+	}
+
+	impl ::std::default::Default for ColorConf {
+		fn default() -> Self {
+			Self {
+				version: 0,
+				header_bg: 	(143, 63, 113),
+				header_fg: 	(255, 255, 255),
+				text_bg: 		(104, 104, 104),
+				text_fg: 		(178, 178, 178),
+			}
+		}
+	}
+
+	impl ColorConf{
+		fn header_bg(&self) -> color::Bg<impl color::Color> {
+			let c = &self.header_bg;
+			color::Bg(color::Rgb(c.0, c.1, c.2))
+		}
+
+		fn header_fg(&self) -> color::Fg<impl color::Color>{
+			let c = &self.header_fg;
+			color::Fg(color::Rgb(c.0, c.1, c.2))
+		}
+
+		fn header_colors(&self) {
+			print!("{}{}", self.header_bg(), self.header_fg())
+		}
+
+		fn text_bg(&self) -> color::Bg<impl color::Color> {
+			let c = &self.text_bg;
+			color::Bg(color::Rgb(c.0, c.1, c.2))
+		}
+
+		fn text_fg(&self) -> color::Fg<impl color::Color>{
+			let c = &self.text_fg;
+			color::Fg(color::Rgb(c.0, c.1, c.2))
+		}
+
+		fn text_colors(&self) {
+			print!("{}{}", self.text_bg(), self.text_fg())
+		}
 
 
-	fn print_header(s: &str, size: usize) {
-		let bg = color::Bg(color::Rgb(143, 63, 113));
+	}
+
+	fn print_header(s: &str, size: usize) -> Result <(), Box<dyn Error>> {
+		let cfg:ColorConf = confy::load("tootdo")?;
+
 		let mut spaces = String::with_capacity(size - s.chars().count());
 		for _ in 0..spaces.capacity(){
 			spaces.push(' ');
 		}
-
-		print!("{}{}{}", bg, s, spaces);
+		cfg.header_colors();
+		print!("{}{}", s, spaces);
+		cfg.text_colors();
+		Ok(())
 	}
 
 
@@ -34,7 +90,7 @@ pub mod cli{
 			print!("{}{}", termion::clear::All, cursor::Goto(1,1) );
 
 			let bg = color::Bg(color::Black);
-			print_header("Todo List", size.0.into());
+			print_header("Todo List", size.0.into())?;
 			println!("{}{}", cursor::Goto(1,1), bg);
 
 			list.print();
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+
+pub mod cli;
 pub mod todolist;
 pub mod text_nav;
-pub mod cli;
 
 fn main() {
 	cli::cli::main();
